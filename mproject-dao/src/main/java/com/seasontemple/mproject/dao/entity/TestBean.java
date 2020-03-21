57049811d72b28f5875e3fcc9df3bd3ef04c468a
SeasonTemple <512743454@qq.com>
2020-03-21 23:48:04 +0800

    adjust project directory structure.

---
+++ mproject-dao/src/main/java/com/seasontemple/mproject/dao/entity/TestBean.java
@@ -0,0 +1,15 @@
+package com.seasontemple.mproject.dao.entity;
+import lombok.Data;
+
+/**
+ * @author Season Temple
+ * @program: mproject
+ * @description: 测试用POJO
+ * @create: 2019/12/21 19:16:28
+ */
+@Data
+public class TestBean {
+    private int id;
+    private String name;
+    private String type;
+}
